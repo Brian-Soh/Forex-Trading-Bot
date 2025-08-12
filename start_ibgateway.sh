91d4ffe6b619a05200bba669f63712bf222be60b
Brian <brian.soh.cs@gmail.com>
2025-08-12 04:41:49 -0700

    Updated the shell script to prevent starting duplicated display instances upon reboot of IB Gateway

--- start_ibgateway.sh
+++ start_ibgateway.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-Xvfb :99 -screen 0 1024x768x24 &
+DISPLAY_NUM=99
+LOCK_FILE="/tmp/.X${DISPLAY_NUM}-lock"
 
-export DISPLAY=:99
+if [ -f "$LOCK_FILE" ]; then
+  LOCK_PID=$(cat "$LOCK_FILE")
+  
+  if ps -p $LOCK_PID > /dev/null 2>&1; then
+    export DISPLAY=:$DISPLAY_NUM
+  else
+    rm -f "$LOCK_FILE"
+    Xvfb :${DISPLAY_NUM} -screen 0 1024x768x24 &
+    export DISPLAY=:$DISPLAY_NUM
+  fi
+else
+  Xvfb :${DISPLAY_NUM} -screen 0 1024x768x24 &
+  export DISPLAY=:$DISPLAY_NUM
+fi
 
 /opt/ibc/gatewaystart.sh &
