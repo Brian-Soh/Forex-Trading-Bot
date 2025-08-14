1b6ca070f429ed01c42851f399187d1f9fd69bf9
Brian <brian.soh.cs@gmail.com>
2025-08-14 03:06:58 -0700

    Update kill_ibgateway.sh to print messages for logging

--- kill_ibgateway.sh
+++ kill_ibgateway.sh
@@ -2,15 +2,24 @@
 
 PIDS=$(ps aux | grep -i "ibgateway" | grep -v grep | awk '{print $2}')
 if [ -n "$PIDS" ]; then
-    kill $PIDS
+    echo "Killing IB Gateway process: $PIDS"
+    kill -9 $PIDS
+else
+    echo "No IB Gateway process found."
 fi
 
 XTERM_PIDS=$(ps aux | grep -i "xterm -T IBC" | grep -v grep | awk '{print $2}')
 if [ -n "$XTERM_PIDS" ]; then
-    kill $XTERM_PIDS
+    echo "Killing IBC launcher: $XTERM_PIDS"
+    kill -9 $XTERM_PIDS
+else
+    echo "No IBC launcher found."
 fi
 
 XVFB_PIDS=$(ps aux | grep -i "Xvfb :99" | grep -v grep | awk '{print $2}')
 if [ -n "$XVFB_PIDS" ]; then
-    kill $XVFB_PIDS
+    echo "Killing Xvfb :99 process: $XVFB_PIDS"
+    kill -9 $XVFB_PIDS
+else
+    echo "No Xvfb :99 process found."
 fi
