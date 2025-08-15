a3153640ac66a8cee5f7e382b724be46cf3f7b28
Brian <brian.soh.cs@gmail.com>
2025-08-14 20:30:28 -0700

    Update kill_ibgateway.sh

--- kill_ibgateway.sh
+++ kill_ibgateway.sh
@@ -3,7 +3,7 @@
 IB_PIDS=$(ps aux | grep -i "ibgateway" | grep -v grep | awk '{print $2}')
 if [ -n "$IB_PIDS" ]; then
     echo "Killing IB Gateway process: $IB_PIDS"
-    kill -9 $IB_PIDS
+    kill $IB_PIDS
 else
     echo "No IB Gateway process found."
 fi
@@ -11,7 +11,7 @@ fi
 XTERM_PIDS=$(ps aux | grep -i "xterm -T IBC" | grep -v grep | awk '{print $2}')
 if [ -n "$XTERM_PIDS" ]; then
     echo "Killing IBC launcher: $XTERM_PIDS"
-    kill -9 $XTERM_PIDS
+    kill $XTERM_PIDS
 else
     echo "No IBC launcher found."
 fi
@@ -19,7 +19,7 @@ fi
 XVFB_PIDS=$(ps aux | grep -i "Xvfb :99" | grep -v grep | awk '{print $2}')
 if [ -n "$XVFB_PIDS" ]; then
     echo "Killing Xvfb :99 process: $XVFB_PIDS"
-    kill -9 $XVFB_PIDS
+    kill $XVFB_PIDS
 else
     echo "No Xvfb :99 process found."
 fi
